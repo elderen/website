64eb05b04ad1996a044fe5417a5a2a7e314d0636
Elderen Lee <elderen2@gmail.com>
2019-08-13 03:13:18 -0400

    added resume jpg and pdf files. allows render when clicked on resume header

--- client/components/resume.jsx
+++ client/components/resume.jsx
@@ -1,17 +1,16 @@
 import React from "react";
+import { useSpring, animated as a } from "react-spring";
 
-export default class Resume extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: 1
-    }
-  }
-  render() {
-    return (
-      <div>
-        <a href="resume.pdf"> Resume </a>
-      </div>
-    )
-  }
+
+const Resume = () => {
+  const [props, set, stop] = useSpring(() => ({ opacity: 0}))
+  set({ opacity: 1 })
+
+  return (
+    <a.div style={props}>
+      <img className="resume" src="resume.jpg" />
+      <a href="resume.pdf" target="_blank">Download Resume</a>
+    </a.div>
+  )
 }
+export default Resume;
